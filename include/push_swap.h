2bbe7efc90d78b6f61905f439f590ad171093335
CarolShingai <carolineshingai@gmail.com>
2024-06-11 21:05:06 -0300

    makefile funciona :D, valiações

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: cshingai <cshingai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 14:43:03 by cshingai          #+#    #+#             */
-/*   Updated: 2024/06/07 14:48:42 by cshingai         ###   ########.fr       */
+/*   Updated: 2024/06/11 20:19:17 by cshingai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,9 @@
 # define PUSH_SWAP_H
 # include "../lib/libft/libft.h"
 # include "../lib/ft_printf/src/ft_printf.h"
+
+# define NOT_INT "Error\n The arguments must be intengers."
+# define ZERO_ARGS ""
+# define DUPLICATE "Error\n There are duplicat arguments."
+# define BIG_MAX "Error\n There is a argument bigger than a intenger"
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,20 +1,51 @@
-#include "push_swap.h"
+#include "../include/push_swap.h"
+#include <limits.h>
+
+void	ft_error(char *msg)
+{
+	ft_printf("%s", msg);
+	exit(0);
+}
+
+void	check_args(char **argv)
+{
+	int idx;
+	int j;
+
+	idx = 1;
+	j = 0;
+	if (!ft_isdigit(*argv[idx]))
+		ft_error(NOT_INT);
+	// ft_printf("1:%s 2:%s", argv[idx], argv[j]);
+	while (argv[idx])
+	{
+		j = idx + 1;
+		if(argv[idx] == argv[j])
+			ft_error(DUPLICATE);
+		idx++;
+	}
+}
+
+void	check_number(char **argv)
+{
+	int idx;
+	int number;
+
+	idx = 0;
+	while (argv[idx])
+	{
+		number = ft_atoi(argv[idx]);
+		if (number == INT_MAX)
+			ft_error(BIG_MAX);
+	}
+}
+
 
 int	main(int argc, char **argv)
 {
+	if (argc < 2)
+		ft_error(ZERO_ARGS);
 	check_args(argv);
-}
-
-int	check_args(char **argv)
-{
-	int i;
-
-	i = 0;
-	if (!ft_isdigit(&argv[i]))
-		ft_error();
-}
-
-void	ft_error(void)
-{
-	exit(0);
+	check_number(argv);
+	return(0);
 }
